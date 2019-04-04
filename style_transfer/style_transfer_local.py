1cd8beac4f813d306f3f8e0eda53679df657ca4e
Mat <msalvaris@users.noreply.github.com>
2019-04-04 15:46:16 +0100

    First version of repo

---
+++ style_transfer/style_transfer_local.py
@@ -0,0 +1,13 @@
+import logging.config
+import fire
+from style_transfer import dask_pipeline
+import os
+
+def run(scheduler_address, model_dir, style, filepath, output_path):
+    logging.config.fileConfig(os.getenv("LOG_CONFIG", "logging.ini"))
+
+    dask_pipeline.start(model_dir, style, filepath, output_path, scheduler_address)
+
+
+if __name__ == "__main__":
+    fire.Fire(run)
